94b316e819285a3e29c941d5397bd1ad586497c5
Clara Nguyen <idesty@derpg.xyz>
2017-11-18 16:53:52 -0500

    Cleaned up

---
+++ pa4/input/tester.c
@@ -0,0 +1,11 @@
+main(int a, int b) {
+	double d;
+	int i;
+	printf("%d %f %d %d\n", "ya");
+	for (d = 0; d < 6; d += 1) {
+
+	}
+	/*while (i == 1) {
+		d += 1;
+	}*/
+}
--- pa4/tester.c
+++
@@ -1,5 +0,0 @@
-main(int a, int b) {
-	double d;
-	int i;
-	printfz("%d %f %d %d\n", "ya");
-}
